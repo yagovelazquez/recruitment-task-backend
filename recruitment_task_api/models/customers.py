358199141c8f93a36e328fb98b1ac3e12183d59a
Yago Velazquez <yagovelazquezsouzaa@gmail.com>
2023-02-25 22:35:57 +0000

    add created at and updated at

--- recruitment_task_api/models/customers.py
+++ recruitment_task_api/models/customers.py
@@ -7,3 +7,5 @@ class Customers(models.Model):
     username = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

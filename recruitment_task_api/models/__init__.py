3cf86042de1ba666058c320a1080b3d51606c833
Yago Velazquez <yagovelazquezsouzaa@gmail.com>
2023-02-25 22:42:20 +0000

    migrate customer details

--- recruitment_task_api/models/__init__.py
+++ recruitment_task_api/models/__init__.py
@@ -1,1 +1,2 @@
 from .customer import *
+from .customer_details import *
---
+++ recruitment_task_api/models/customer_details.py
@@ -0,0 +1,16 @@
+from django.db import models
+from recruitment_task_api.models.customer import Customer
+
+class CustomerDetails(models.Model):
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    picture = models.URLField()
+    age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=10)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    company = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    about = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
